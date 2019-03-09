3a45b5c366c615e392ac3c107beceb48383367c1
tornworm <42328527+tornworm@users.noreply.github.com>
2019-03-09 22:51:37 +0800

    new project framework
    this is a new project framework by tw

---
+++ trunk/035-mhxy/Assets/Scripts/BaseWindows.cs
@@ -0,0 +1,15 @@
+﻿//this is a C#
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class BaseWindows : MonoBehaviour
+{
+   
+}
